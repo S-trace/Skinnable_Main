012aad97ef3e580050da417ce47f6c3125e6e73d
S-trace <S-trace@list.ru>
2017-09-26 01:54:16 +0300

    [SKINNABLE] Allow skin resources to be loaded from main app

--- app/src/main/java/s_trace/mainapp/LoginActivity.java
+++ app/src/main/java/s_trace/mainapp/LoginActivity.java
@@ -2,6 +2,7 @@ package s_trace.mainapp;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
@@ -19,11 +20,21 @@ public class LoginActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
-        mLoginView = findViewById(R.id.login);
-        mPasswordView = findViewById(R.id.password);
 
-        Button mShowLoginButton = findViewById(R.id.show_login_button);
+        // Initialize SkinResources
+        SkinResources.setSkinPackage("s_trace.skin");
+        SkinResources.initialize(getApplicationContext());
+
+        // We must inflate layout within skin context
+        LayoutInflater li = LayoutInflater.from(SkinResources.getsSkinContext());
+        View view = li.inflate(SkinResources.getLayout(R.layout.activity_login), null);
+        setContentView(view);
+
+        // And must take layout elements IDs within skin context too (or app will crash during this activity startup)
+        mLoginView = findViewById(SkinResources.getIdentifier(R.id.login));
+        mPasswordView = findViewById(SkinResources.getIdentifier(R.id.password));
+
+        Button mShowLoginButton = findViewById(SkinResources.getIdentifier(R.id.show_login_button));
         mShowLoginButton.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -31,7 +42,7 @@ public class LoginActivity extends Activity {
             }
         });
 
-        Button mShowPasswordButton = findViewById(R.id.show_password_button);
+        Button mShowPasswordButton = findViewById(SkinResources.getIdentifier(R.id.show_password_button));
         mShowPasswordButton.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -40,13 +51,14 @@ public class LoginActivity extends Activity {
         });
     }
 
+    // Resources which is not present in skin package are loaded from main app package
     private void showLogin() {
-        Toast.makeText(this, getString(R.string.login) + mLoginView.getText().toString(),
+        Toast.makeText(this, SkinResources.getString(R.string.login) + mLoginView.getText().toString(),
                 Toast.LENGTH_SHORT).show();
     }
 
     private void showPassword() {
-        Toast.makeText(this, getString(R.string.password) + mPasswordView.getText().toString(),
+        Toast.makeText(this, SkinResources.getString(R.string.password) + mPasswordView.getText().toString(),
                 Toast.LENGTH_SHORT).show();
     }
 }
